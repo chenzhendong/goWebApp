a00aab7f553efe111d32eb5970e663a851819eea
Zhendong Chen <chenzhendong@yahoo.com>
2016-01-20 13:55:13 -0500

    add sqlite support

--- auth/models/base.go
+++ auth/models/base.go
@@ -2,12 +2,35 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	//_ "github.com/mattn/go-sqlite3"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 var DB gorm.DB
 
 func init() {
+	DB = initPostgres()
+
+	DB.SingularTable(true)
+	DB.DB().SetMaxIdleConns(10)
+	DB.DB().SetMaxOpenConns(100)
+
+	DB.DropTableIfExists(new(User), new(Profile), new(Address))
+	DB.CreateTable(new(User), new(Profile), new(Address))
+	DB.LogMode(true)
+}
+
+func initSqlite() gorm.DB  {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal("Failed to connect to Sqlite DB.", err)
+	}
+	return db
+}
+
+func initPostgres()  gorm.DB {
+
 	dbhost := "localhost"
 	dbport := "5432"
 	dbuser := "nodeframe"
@@ -19,17 +42,9 @@ func init() {
 
 	db, err := gorm.Open("postgres", "postgres", "host=" + dbhost + " user=" + dbuser + " password='" + dbpassword + "' dbname=" + dbname + " port=" + dbport + " sslmode=disable")
 	if err != nil {
-
+		log.Fatal("Failed to connect to Postgres DB.", err)
 	}
-	
-	db.DB().SetMaxIdleConns(10)
-	db.DB().SetMaxOpenConns(100)
-	db.SingularTable(true)
 
-	db.DropTableIfExists(&User{}, &Profile{}, &Address{})
-	db.CreateTable(&User{}, &Profile{}, &Address{})
-	//db.LogMode(true)
-
-	DB = db
+	return db
 }
 
